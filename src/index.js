2b8f65aa9983db4cb9114bf5ffbd2a1ab65ec599
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-02-25 17:13:31 +0100

    Fetch weather data and display

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import "./styles.css";
-console.log("hello world!");
+
+const locationNode = document.querySelector(".location");
+const temperatureNode = document.querySelector(".temperature");
+const weatherDescNode = document.querySelector(".weather-desc");
+
+async function fetchWeatherData(searchLoc) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?appid=610c66bbb97cde2f3b16c870a3f539df&q=${searchLoc}&units=metric`,
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  return {
+    location: weatherData.name,
+    temperature: weatherData.main.temp,
+    description: weatherData.weather[0].main,
+  };
+}
+
+fetchWeatherData("Dortmund").then((weatherObj) => {
+  locationNode.innerText = `Location: ${weatherObj.location}`;
+  temperatureNode.innerText = `Temperature: ${weatherObj.temperature}°`;
+  weatherDescNode.innerText = `Weather: ${weatherObj.description}`;
+});
